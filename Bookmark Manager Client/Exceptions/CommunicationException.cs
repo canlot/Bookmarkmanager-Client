9f5c3d20899076847372d3dba09f04ab852df517
canlot <jakobsasmuzki@gmx.de>
2025-05-11 07:45:21 +0200

    Added Tooltip

---
+++ Bookmark Manager Client/Exceptions/CommunicationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookmark_Manager_Client.Exceptions
+{
+    public class CommunicationException : Exception
+    {
+        public string ServerMessage;
+        public string InternalMessage;
+        public CommunicationException() { }
+    }
+}
