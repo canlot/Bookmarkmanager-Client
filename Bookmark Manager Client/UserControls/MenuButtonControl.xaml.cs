75d63a1449bde21116fce74c602a13a77c23aa12
cansthing <josua@l-en.de>
2025-06-20 19:21:00 +0200

    Adding MenuButtonControl - implemented in MainWindow

---
+++ Bookmark Manager Client/UserControls/MenuButtonControl.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace Bookmark_Manager_Client.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MenuButtonControl.xaml
+    /// </summary>
+    public partial class MenuButtonControl : UserControl
+    {
+        public DrawingImage Icon
+        {
+            get { return (DrawingImage)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(DrawingImage), typeof(MenuButtonControl), new PropertyMetadata(null));
+
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(MenuButtonControl), new PropertyMetadata(null));
+
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(MenuButtonControl), new PropertyMetadata(null));
+
+
+
+        public string CommandParameter
+        {
+            get { return (string)GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.Register("CommandParameter", typeof(string), typeof(MenuButtonControl), new PropertyMetadata(null));
+
+
+
+
+
+        public MenuButtonControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
